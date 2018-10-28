a2ee283727b57b19f3ac8c29766141f12c2e1eee
Konstantin Bukin <bukin@inetstudio.ru>
2018-10-28 04:10:12 +0300

    Remove observers

--- entities/profiles/src/Providers/ProfilesBindingsServiceProvider.php
+++ entities/profiles/src/Providers/ProfilesBindingsServiceProvider.php
@@ -20,13 +20,9 @@ class ProfilesBindingsServiceProvider extends ServiceProvider
     public $bindings = [
         'InetStudio\ACL\Profiles\Contracts\Models\UserProfileModelContract' => 'InetStudio\ACL\Profiles\Models\UserProfileModel',
         'InetStudio\ACL\Profiles\Contracts\Models\UserSocialProfileModelContract' => 'InetStudio\ACL\Profiles\Models\UserSocialProfileModel',
-        'InetStudio\ACL\Profiles\Contracts\Observers\UserProfileObserverContract' => 'InetStudio\ACL\Profiles\Observers\UserProfileObserver',
-        'InetStudio\ACL\Profiles\Contracts\Observers\UserSocialProfileObserverContract' => 'InetStudio\ACL\Profiles\Observers\UserSocialProfileObserver',
         'InetStudio\ACL\Profiles\Contracts\Repositories\UsersProfilesRepositoryContract' => 'InetStudio\ACL\Profiles\Repositories\UsersProfilesRepository',
         'InetStudio\ACL\Profiles\Contracts\Repositories\UsersSocialProfilesRepositoryContract' => 'InetStudio\ACL\Profiles\Repositories\UsersSocialProfilesRepository',
-        'InetStudio\ACL\Profiles\Contracts\Services\Front\UsersProfilesObserverServiceContract' => 'InetStudio\ACL\Profiles\Services\Front\UsersProfilesObserverService',
         'InetStudio\ACL\Profiles\Contracts\Services\Front\UsersProfilesServiceContract' => 'InetStudio\ACL\Profiles\Services\Front\UsersProfilesService',
-        'InetStudio\ACL\Profiles\Contracts\Services\Front\UsersSocialProfilesObserverServiceContract' => 'InetStudio\ACL\Profiles\Services\Front\UsersSocialProfilesObserverService',
         'InetStudio\ACL\Profiles\Contracts\Services\Front\UsersSocialProfilesServiceContract' => 'InetStudio\ACL\Profiles\Services\Front\UsersSocialProfilesService',
     ];
 
